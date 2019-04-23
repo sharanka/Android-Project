bccbf4884a383f78f35b1f8c0508f5ea3e742828
NICOLAI-PC\nicolai <nicolai@matthiesen4.dk>
2019-04-23 13:32:52 +0200

    Recycle

--- app/src/main/java/andapp/bachGroup/androidproject/MainActivity.java
+++ app/src/main/java/andapp/bachGroup/androidproject/MainActivity.java
@@ -1,8 +1,11 @@
 package andapp.bachGroup.androidproject;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,24 +16,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView theRecyclerView;
+    private RecyclerView.Adapter theAdapter;
+    private RecyclerView.LayoutManager theLayoutManager;
+    private Random random;
+
+    private ArrayList<Integer> intArray;
+    private ArrayList<Image> imageArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        random = new Random();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,8 +48,34 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+       theRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        theRecyclerView.setHasFixedSize(true);
+
+        theLayoutManager = new GridLayoutManager(this, 2);
+        theRecyclerView.setLayoutManager(theLayoutManager);
+
+        intArray = generateIntegerArrayList(200);
+
+        theAdapter = new MovieAdapter(intArray);
+        theRecyclerView.setAdapter(theAdapter);
     }
 
+    private ArrayList<Integer> generateIntegerArrayList(int amount){
+        ArrayList<Integer> radom = new ArrayList<>();
+        for (int i = 0; i<amount; i++){
+            radom.add(random.nextInt(10000));
+        }
+        return radom;
+    }
+
+    private ArrayList<Image> fetchImages(){
+        ArrayList<Image> temp = new ArrayList<>();
+
+
+        return temp;
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
