1c122aebf9ad7027ce6160e77b096fb12279fd93
SanderumChristian <chund.Christianjohansen@gmail.com>
2019-04-23 09:46:21 +0200

    Start of project

---
+++ AndroidProject/app/src/main/java/com/example/android_project/RecycleViewAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android_project;
+
+
+import static com.example.android_project.RecycleViewAdapter.*;
+
+public class RecycleViewAdapter extends RecycleView.Adapter<ViewHolder>{
+
+}
